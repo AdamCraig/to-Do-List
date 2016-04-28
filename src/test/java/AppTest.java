34d3817bdce7693686f94bd9572a7a38c471148a
Adam Craig and Ashley Maceli <ashley@>
2016-04-28 11:57:10 -0700

    add get route and corresponding category-tasks-form.vtl, add post route and modified tasks.vtl to display all tasks within category. Add passing integration test for tasks being added and displayed.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -56,4 +56,30 @@ public class AppTest extends FluentTest {
     click("a", withText("Household chores"));
     assertThat(pageSource()).contains("Household chores");
   }
+
+  @Test
+  public void categoryTasksFormIsDisplayed() {
+    goTo("http://localhost:4567/categories/new");
+    fill("#name").with("Shopping");
+    submit(".btn");
+    click("a", withText("View categories"));
+    click("a", withText("Shopping"));
+    click("a", withText("Add a new task"));
+    assertThat(pageSource()).contains("Add a task to Shopping");
+  }
+
+  @Test
+  public void tasksIsAddedAndDisplayed() {
+    goTo("http://localhost:4567/categories/new");
+    fill("#name").with("Banking");
+    submit(".btn");
+    click("a", withText("View categories"));
+    click("a", withText("Banking"));
+    click("a", withText("Add a new task"));
+    fill("#description").with("Deposit paycheck");
+    submit(".btn");
+    click("a", withText("View categories"));
+    click("a", withText("Banking"));
+    assertThat(pageSource()).contains("Deposit paycheck");
+  }
 }
